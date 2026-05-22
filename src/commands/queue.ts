acc229411bf2e630347dbcbeb74d844d20f57560
dadrxid <andrewdrussell01@gmail.com>
2026-05-22 14:09:33 +0100

    fix: remove as any from action rows

--- src/commands/queue.ts
+++ src/commands/queue.ts
@@ -44,7 +44,7 @@ export default class implements Command {
 
     const msg = await interaction.reply({
       embeds: [embed],
-      components: maxPage > 1 ? [buttons.toJSON()] : [],
+      components: maxPage > 1 ? [buttons] : [],
       fetchReply: true,
     });
 
@@ -66,7 +66,7 @@ export default class implements Command {
 
       await i.update({
         embeds: [newEmbed],
-        components: [newButtons.toJSON()],
+        components: [newButtons],
       });
     });
 
